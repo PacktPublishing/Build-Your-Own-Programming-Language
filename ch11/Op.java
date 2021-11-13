e0a4c1fd27169d1d04a2406392c0b488318c7317
Clinton <clint.jeffery@gmail.com>
2021-11-13 00:40:57 -0700

    add Chapter 11 code

---
+++ ch11/Op.java
@@ -0,0 +1,8 @@
+public class Op {
+  public final static short HALT=1, NOOP=2, ADD=3, SUB=4,
+    MUL=5, DIV=6, MOD=7, NEG=8, PUSH=9, POP=10, CALL=11,
+    RETURN=12, GOTO=13, BIF=14, LT=15, LE=16, GT=17, GE=18,
+    EQ=19, NEQ=20, LOCAL=21, LOAD=22, STORE=23;
+  public final static short R_NONE=0, R_ABS=1, R_IMM=2,
+    R_STACK=3, R_HEAP=4;
+}
---
+++ ch11/hello.java
@@ -0,0 +1,9 @@
+public class hello {
+   public static void main(String argv[]) {
+      int x = argv.length;
+      x = x + 2;
+      if (x > 3) {
+         System.out.println("hello, jzero!");
+      }
+   }
+}
